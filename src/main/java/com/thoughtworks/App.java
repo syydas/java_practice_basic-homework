2d7e84adf93cc7be3641bc0dc78043b5f6ccb8f6
Asuu <suqinzheng_0624@163.com>
2020-03-01 16:36:10 +0800

    [Asu]: feat: creat a class Teacher and find repeat students

--- src/main/java/com/thoughtworks/App.java
+++ src/main/java/com/thoughtworks/App.java
@@ -1,7 +1,20 @@
 package com.thoughtworks;
 
+import java.text.ParseException;
+import java.util.ArrayList;
+
 public class App {
 
-  public static void main(String[] args) {
-  }
+    public static void main(String[] args) throws ParseException {
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student("张三", "001", "2019.02.11"));
+        students.add(new Student("李四", "002", "2019.02.10"));
+        students.add(new Student("王五", "003", "2019.02.09"));
+
+        Teacher teacher = new Teacher("张龙", students);
+        ArrayList<Student> repeatStu = teacher.checkInfo();
+        for (Student student : repeatStu) {
+            System.out.println(student);
+        }
+    }
 }
--- src/main/java/com/thoughtworks/Teacher.java
+++ src/main/java/com/thoughtworks/Teacher.java
@@ -1,6 +1,28 @@
 package com.thoughtworks;
 
+import java.util.ArrayList;
+
 public class Teacher {
     private String name;
+    private ArrayList<Student> studentList;
 
+    public Teacher(String name, ArrayList<Student> studentList) {
+        this.name = name;
+        this.studentList = studentList;
+    }
+
+    public Teacher() {
+    }
+
+    public ArrayList<Student> checkInfo() {
+        ArrayList<Student> repeatStudent = new ArrayList<>();
+        for (int i = 0; i < studentList.size(); i++) {
+            for (int j = i + 1; j < studentList.size(); j++) {
+                if (studentList.get(i).equals(studentList.get(j))) {
+                    repeatStudent.add(studentList.get(i));
+                }
+            }
+        }
+        return repeatStudent;
+    }
 }
